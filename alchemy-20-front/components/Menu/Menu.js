53002da958f234e7a7a862ffa9e90f68675df370
shambu2k <sidharthshambu00@gmail.com>
2020-09-20 18:51:16 +0530

    style: Side navigation minor styling

--- alchemy-20-front/components/Menu/Menu.js
+++ alchemy-20-front/components/Menu/Menu.js
@@ -6,6 +6,9 @@ import Link from 'next/link'
 export default function Menu({ open }) {
   return (
     <StyledMenu open={open}>
+      <img src='/alchemy_2018.png' alt="Alchemy Logo"></img>
+      <a>ALCHEMY'20</a>
+      <h4>OCTOBER 18<sup>th</sup> - 20<sup>th</sup>, 2020</h4>
       <Link href="/">
         <p>About</p>
       </Link>
--- alchemy-20-front/components/Menu/Menu.styled.js
+++ alchemy-20-front/components/Menu/Menu.styled.js
@@ -4,6 +4,7 @@ export const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   background: #303030;
   height: 100vh;
   text-align: left;
@@ -13,26 +14,38 @@ export const StyledMenu = styled.nav`
   left: 0;
   transition: transform 0.3s ease-in-out;
   transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+
+  img {
+    height: 9em;
+    width: 9em;
+  }
+
+  a {
+    color: #ecc82c;
+    margin-top: 1rem;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: bold;
+  }
+  h4 {
+    color: white;
+    font-family: 'Josefin Sans', sans-serif;
+  }
   
   @media (max-width: 600px) {
     width: 100%;
   }
 
   p {
-    font-size: 2rem;
+    font-size: 1.5rem;
     text-transform: uppercase;
     letter-spacing: 0.5rem;
-    color: #ecc82c;
+    color: white;
     text-decoration: none;
     transition: color 0.3s linear;
-    
-    @media (max-width: 600px) {
-      font-size: 1.5rem;
-      text-align: center;
-    }
 
     &:hover {
-      color: white;
+      color: #ecc82c;
+      cursor: pointer;
     }
   }
 `;
