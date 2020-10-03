306553563d1a8e1791072ad90a7b776772b31701
madhu2000u <madhu2000u@gmail.com>
2020-10-03 23:12:39 +0530

    Update: All events CRUD done

--- backend/nodeServer/models/users.js
+++ backend/nodeServer/models/users.js
@@ -2,7 +2,7 @@
 
 const mongoose=require('mongoose')
 
-const userSchema=new mongoose.Schema({
+const userSchema=new mongoose.Schema({          //_id is created automatically by mongoose
     alc_id: {type: String, required:true, unique:true},
     email: {type:String, required: true, unique:true},
     hashed_password: String,
