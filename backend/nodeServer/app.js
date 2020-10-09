fbbcd76a3e5062ba06e5ff54d0c52f20ab21bca8
madhu2000u <madhu2000u@gmail.com>
2020-10-09 18:28:39 +0530

    Azure deployment, mongodb atlas code added

--- backend/nodeServer/app.js
+++ backend/nodeServer/app.js
@@ -12,7 +12,8 @@ app.use(express.json())
 app.use('/api', require('./routes/auth_routes'))
 app.use('/api', require('./routes/main_routes'))
 
-mongoose.connect('mongodb://localhost:27017/alchemy-20-db', ({useNewUrlParser:true, useUnifiedTopology:true})).then(()=>{console.log('Connected to alchemy db')}).catch((err)=>{console.log('Error connection to db - ', err)})
+//mongoose.connect('mongodb://localhost:27017/alchemy-20-db', ({useNewUrlParser:true, useUnifiedTopology:true})).then(()=>{console.log('Connected to alchemy db')}).catch((err)=>{console.log('Error connection to db - ', err)})
+mongoose.connect(`mongodb+srv://alchemy-db-madhu:${process.env.my_db_pass}@alchemy-20.xrhio.gcp.mongodb.net/alchemy-20?retryWrites=true&w=majority`, ({useNewUrlParser:true, useUnifiedTopology:true})).then(()=>{console.log('Connected to alchemy db')}).catch((err)=>{console.log('Error connection to db - ', err)})
 // mongoose.connection.db.listCollections({name: "api_keys"}).next((err, info)=>{
 //     if(!info){
 //         post:'',
@@ -20,4 +21,4 @@ mongoose.connect('mongodb://localhost:27017/alchemy-20-db', ({useNewUrlParser:tr
 //     }
 // })
 
-app.listen(process.env.process || default_port, ()=>{console.log('Server listening...')} )
+app.listen(process.env.PORT || default_port, ()=>{console.log('Server listening...')} )
