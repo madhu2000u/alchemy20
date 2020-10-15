fff283ea7faf246122a4c1d91abb2c5d376d6f89
shambu2k <sidharthshambu00@gmail.com>
2020-10-16 02:47:07 +0530

    feat: add docker

--- backend/nodeServer/app.js
+++ backend/nodeServer/app.js
@@ -1,23 +1,28 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 // const key=require('./models/api_keys')
-const app=express()
-const default_port=3000
+const app = express();
+const default_port = 4700;
 
-dotenv.config()
+dotenv.config();
 
+app.use(express.json());
+app.use("/api", require("./routes/auth_routes"));
+app.use("/api", require("./routes/main_routes"));
 
-app.use(express.json())
-app.use('/api', require('./routes/auth_routes'))
-app.use('/api', require('./routes/main_routes'))
+mongoose
+  .connect("mongodb://database:27017/alchemy-20-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to alchemy db");
+  })
+  .catch((err) => {
+    console.log("Error connection to db - ", err);
+  });
 
-mongoose.connect('mongodb://localhost:27017/alchemy-20-db', ({useNewUrlParser:true, useUnifiedTopology:true})).then(()=>{console.log('Connected to alchemy db')}).catch((err)=>{console.log('Error connection to db - ', err)})
-// mongoose.connection.db.listCollections({name: "api_keys"}).next((err, info)=>{
-//     if(!info){
-//         post:'',
-
-//     }
-// })
-
-app.listen(process.env.process || default_port, ()=>{console.log('Server listening...')} )
+app.listen(process.env.process || default_port, () => {
+  console.log("Server listening at PORT:"+process.env.process || default_port3);
+});
