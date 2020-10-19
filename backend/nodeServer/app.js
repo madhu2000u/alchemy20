0acdab0d70132ee428b9cf65ee3201e781397bd5
shambu2k <sidharthshambu00@gmail.com>
2020-10-19 11:44:40 +0530

    Revert "fix: docker and no docker support"
    This reverts commit bd0fd486828cfcec9b6b8533f6846f144fdaee89.

--- backend/nodeServer/app.js
+++ backend/nodeServer/app.js
@@ -1,33 +1,29 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+// const key=require('./models/api_keys')
 const app = express();
 const default_port = 4700;
+
+
+dotenv.config();
 
 app.use(express.json());
 app.use("/api", require("./routes/auth_routes"));
 app.use("/api", require("./routes/main_routes"));
 
+
 mongoose
-  .connect(
-    process.env.DB_PRODUCTION || "mongodb://localhost:27017/alchemy-20-db",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to alchemy db");
   })
   .catch((err) => {
-    console.log(
-      "Error connection to db - " + process.env.DB_PRODUCTION ||
-        "mongodb://localhost:27017/alchemy-20-db",
-      err
-    );
+    console.log("Error connection to db - ", err);
   });
 
-app.listen(process.env.PORT || default_port, () => {
-  console.log("Server listening...");
-});
+app.listen(process.env.PORT || default_port, ()=>{console.log('Server listening...')} )
