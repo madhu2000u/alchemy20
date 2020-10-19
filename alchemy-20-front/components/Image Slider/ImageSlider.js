100fc4d3949535bb70c9b857119926af1aecc6bb
shambu2k <sidharthshambu00@gmail.com>
2020-10-19 16:47:35 +0530

    fix: image slider center

--- alchemy-20-front/components/Image Slider/ImageSlider.js
+++ alchemy-20-front/components/Image Slider/ImageSlider.js
@@ -21,7 +21,7 @@ export default function ImageSlider({ images }) {
     },
     tablet: {
       breakpoint: {
-        max: 1024,
+        max: 1345,
         min: 730,
       },
       items: 2,
@@ -30,7 +30,6 @@ export default function ImageSlider({ images }) {
   }
 
   return (
-    <div className={styles.slider}>
       <Carousel
         arrows={false}
         autoPlay={true}
@@ -49,6 +48,7 @@ export default function ImageSlider({ images }) {
         swipeable={false}
         minimumTouchDrag={0}
         draggable={false}
+        containerClass={styles.slider}
       >
         {images.map((image) => (
           <div key={image._id}>
@@ -56,6 +56,5 @@ export default function ImageSlider({ images }) {
           </div>
         ))}
       </Carousel>
-    </div>
   );
 }
