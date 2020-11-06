55b1c202d794a3bd52f923a444bb5ef274caa0d2
geforce6t <shashwatmessi43@gmail.com>
2020-11-06 23:38:06 +0530

    [Refactor]: Format code, Add script for format.

--- backend/nodeServer/app.js
+++ backend/nodeServer/app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 // const key=require('./models/api_keys')
 const app = express();
 const default_port = 4700;
 
-
 dotenv.config();
 
 app.use(express.json());
-app.use("/api", require("./routes/auth_routes"));
-app.use("/api", require("./routes/main_routes"));
-
+app.use('/api', require('./routes/auth_routes'));
+app.use('/api', require('./routes/main_routes'));
 
 mongoose
-  .connect(process.env.db_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to alchemy db");
-  })
-  .catch((err) => {
-    console.log("Error connection to db - ", err);
-  });
+	.connect(process.env.db_url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Connected to alchemy db');
+	})
+	.catch((err) => {
+		console.log('Error connection to db - ', err);
+	});
 
-app.listen(process.env.PORT || default_port, ()=>{console.log('Server listening...')} )
+app.listen(process.env.PORT || default_port, () => {
+	console.log('Server listening...');
+});
--- backend/nodeServer/models/users.js
+++ backend/nodeServer/models/users.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
-const userSchema=new mongoose.Schema({          //_id is created automatically by mongoose
-    alc_id: {type: String, required:true, unique:true},
-    email: {type:String, required: true, unique:true},
-    hashed_password: String,
-    name: String,
-    mobile: String,
-    college: String,
-    dept: String,
-    year_of_study: String,
-    accommodation: Boolean,
-    acc_active: Boolean,
-    registered_events: [String]
-
-})
+const userSchema = new mongoose.Schema({
+	//_id is created automatically by mongoose
+	alc_id: {type: String, required: true, unique: true},
+	email: {type: String, required: true, unique: true},
+	hashed_password: String,
+	name: String,
+	mobile: String,
+	college: String,
+	dept: String,
+	year_of_study: String,
+	accommodation: Boolean,
+	acc_active: Boolean,
+	registered_events: [String],
+});
 
 //mongoose.connect('mongodb://localhost:27017/alchemy-20-db', ({useNewUrlParser:true, useUnifiedTopology:true})).then(()=>{console.log('Connected to users alchemy db')}).catch((err)=>{console.log('Error connection to db - ', err)})
 
-module.exports=mongoose.model('users', userSchema)
+module.exports = mongoose.model('users', userSchema);
