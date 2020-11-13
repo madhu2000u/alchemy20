b2a4c4ef73c63558e4c312bbc74b7a6a98a63b52
geforce6t <shashwatmessi43@gmail.com>
2020-11-13 15:23:08 +0530

    [refactor]: Filter only existing events

--- backend/nodeServer/controllers/RegisterEventController.js
+++ backend/nodeServer/controllers/RegisterEventController.js
@@ -8,6 +8,7 @@ the following operations may suffice :
 
 */
 const RegisteredEvent = require('../models/registered_students');
+const Event = require('../models/events');
 
 //get all the registered events of a user
 exports.getRegisteredEvents = (req, res) => {
@@ -33,28 +34,39 @@ exports.pushRegisteredEvents = (req, res) => {
 	//expecting client to send the string event id and user_id as a header
 	const event_id = req.headers['event_id'];
 	const _id = req.headers['user_id'];
-	if (!_id || !event_id) {
-		res.status(400).json({message: 'Required fields missing'});
-	} else {
-		RegisteredEvent.findOne({user_id: _id}, (err, result) => {
-			if (err) {
-				res.status(500).json({message: 'Internal server error'});
-			} else {
-				if (result.events.includes(event_id)) {
-					res.status(409).json({message: 'Already registered for the Event'});
+	Event.exists({_id: event_id})
+		.then((result) => {
+			if (result) {
+				if (!_id) {
+					res.status(400).json({message: '* Required field missing'});
 				} else {
-					RegisteredEvent.updateOne({user_id: _id}, {$push: {events: event_id}}, (err, raw) => {
+					RegisteredEvent.findOne({user_id: _id}, (err, result) => {
 						if (err) {
-							console.log(err);
 							res.status(500).json({message: 'Internal server error'});
 						} else {
-							res.status(200).json({message: 'Sucessfully registered for the Event !'});
+							if (result.events.includes(event_id)) {
+								res.status(409).json({message: 'Already registered for the Event'});
+							} else {
+								RegisteredEvent.updateOne({user_id: _id}, {$push: {events: event_id}}, (err, raw) => {
+									if (err) {
+										console.log(err);
+										res.status(500).json({message: 'Internal server error'});
+									} else {
+										res.status(200).json({message: 'Sucessfully registered for the Event !'});
+									}
+								});
+							}
 						}
 					});
 				}
+			} else {
+				res.status(400).json({message: 'Event not present'});
 			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(422).json({message: 'Invalid or null Id provided'});
 		});
-	}
 };
 
 exports.removeRegisteredEvent = (req, res) => {
