e39772c394f7f17bb97e950a6f75c4fc098034a6
shashwat <63119158+geforce6t@users.noreply.github.com>
2021-01-12 02:18:08 +0530

    [feat]: dashboard api (#35)

--- backend/nodeServer/routes/dashboardRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/DashboarController');
-const utils = require('../functions/authUtils');
-
-router.get('/dashboard', utils.jwtVerify, DashboardController.dashboard); //utils.jwtVerify essentially verifies if user signed in or notifController
-
-module.exports = router;
