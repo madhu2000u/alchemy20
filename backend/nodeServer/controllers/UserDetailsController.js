3c35d05b49572a5a1fffb514fab0ea1012109b05
madhu2000u <madhu2000u@gmail.com>
2021-01-12 15:12:48 +0530

    feat: User details filling, user details availability checks

--- backend/nodeServer/controllers/UserDetailsController.js
+++ backend/nodeServer/controllers/UserDetailsController.js
@@ -1,19 +1,48 @@
 const User = require('../models/users');
 
 exports.AddUserDetails = (req, res) => {
-	console.log('Inside user details controller req - ', req.user, req.body);
-	const body = req.body;
+    console.log("Inside user details controller req - ",req.user, req.body);
+    const body = req.body
 
-	if (!body.name || !body.mobile || !body.college || !body.dept || !body.year_of_study) {
-		res.status(401).json({message: 'Required fields missing'});
-	}
+    if(!body.name || !body.mobile || !body.college || !body.dept || !body.year_of_study){res.status(401).json({message:"Required fields missing"})}
 
-	User.update({_id: req.user.id}, body)
-		.then((result) => {
-			console.log('AddUserDetails result: ' + result);
-			res.sendStatus(200);
-		})
-		.catch((err) => {
-			res.status(401).json({error: err});
-		});
-};
+    User.findOne({_id:req.user.id}).then((result)=>{
+        if(!result){
+            res.status(403).json({message:"Invalid auth token or user doesn't exist. Please login again"})
+        }
+        else if(result.mobile){
+            console.log(result.mobile)
+            res.status(409).json({message:"Already filled"})
+        }
+        else{
+            User.updateOne({_id:req.user.id}, body).then((result) =>{
+                console.log("AddUserDetails result: " + result)
+                res.sendStatus(200)
+        
+            }).catch((err) =>{
+                res.status(401).json({error:err})
+        
+            })
+        }
+    })
+    
+   
+
+}
+
+exports.checkUserDetails = (req, res, next) =>{
+    User.findOne({_id:req.user.id}).then((result)=>{
+        if(!result){
+            res.status(403).json({message:"Invalid auth token or user doesn't exist. Please login again"})
+        }
+        else if(result.mobile){
+            console.log(result.mobile)
+            next()
+        }
+        else{
+            res.status(404).json({message:"Fill details before event/workshop registration"})
+
+        }
+
+})
+}
--- backend/nodeServer/routes/registerEventRoutes.js
+++ backend/nodeServer/routes/registerEventRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const registerEventController = require('../controllers/RegisterEventController');
+const userDetailsController = require('../controllers/UserDetailsController');
 const utils = require('../functions/authUtils');
 
-router.post('/registerEvent', utils.jwtVerify, registerEventController.pushRegisteredEvents);
-router.get('/registerEvent', utils.jwtVerify, registerEventController.getRegisteredEvents);
-router.delete('/registerEvent', utils.jwtVerify, registerEventController.removeRegisteredEvent);
+router.post('/registerEvent', utils.jwtVerify, userDetailsController.checkUserDetails,  registerEventController.pushRegisteredEvents);
+router.get('/registerEvent', utils.jwtVerify, userDetailsController.checkUserDetails, registerEventController.getRegisteredEvents);
+router.delete('/registerEvent', utils.jwtVerify, userDetailsController.checkUserDetails, registerEventController.removeRegisteredEvent);
 
 module.exports = router;
