6861ecec975980b9c65fe7af131c265209a5b21d
geforce6t <shashwatmessi43@gmail.com>
2021-04-16 14:41:40 +0530

    [feat] : Add magazine page

---
+++ alchemy-20-front/pages/magazines.js
@@ -0,0 +1,28 @@
+import Common from '../components/Common/Common';
+import Subpage from '../components/Subpage Template/Subpage';
+import styles from '../styles/magazine.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Magazine() {
+	return (
+		<main className={styles.main}>
+			<Subpage showNot={false} />
+			<div className={styles.heading}>
+				<p>Magazines</p>
+			</div>
+			<div className={styles.card}>
+				<card className={styles.card_items}>
+					<Link href="https://pdfhost.io/v/c2NmfZmZq_Magazinepdf.pdf" className={styles.link}>
+						<a className={styles.link}>MAGAZINE 1</a>
+					</Link>
+					<Link href="https://pdfhost.io/v/K512X6kBv_mag_febpdf.pdf" className={styles.link}>
+						<a className={styles.link}>MAGAZINE 2</a>
+					</Link>
+				</card>
+			</div>
+		</main>
+	);
+}
+
+Magazine.Layout = Common;
